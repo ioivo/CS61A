54ad59b8680b28088df3395bc846946559e2ab20
ioivo <143081237+ioivo@users.noreply.github.com>
2025-12-04 21:44:48 +0800

    origin

---
+++ HW/hw08/hw08/hw08.scm
@@ -0,0 +1,37 @@
+(define (ascending? s)
+  (cond
+    ((null? s) #t)
+    ((null? (cdr s)) #t)
+    ((> (car s) (car (cdr s))) #f)
+    (else (ascending? (cdr s)))))
+
+(define (my-filter pred s)
+    (cond
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
+
+(define (interleave lst1 lst2)
+    (cond
+    ((and (null? lst1) (null? lst2)) '())
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
+
+(define (no-repeats s)
+  (cond
+    ((null? s) 
+     '())
+    (else
+      (let 
+        ((current (car s))
+         (rest (my-filter (lambda (x) (not (= x current))) (cdr s))))
+        (cons current (no-repeats rest))
+      ) 
+    ) 
+  ) 
+) 
